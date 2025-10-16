a3d364db886d7ee1532e0c52355d903274ca6fc4
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-10-15 20:53:11 -0400

    feat: adiciona schemas para padronizar

--- auth_routes.py
+++ auth_routes.py
@@ -1,27 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from dependencies import get_session
 from models import Usuario
 from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
 async def home():
     """
-
     rota docstring...
-    
     """
     return {"message": "Authentication successful"}
 
 @auth_router.post("/signup")
-async def signup(nome:str,email: str, senha: str, session = Depends(get_session)):
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+async def signup(usuario_schema: UsuarioSchema, session: Session = Depends(get_session)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
-        return {"message": "Usuário já existe"}
+        raise HTTPException(status_code=400, detail="Usuário cadastrado")
     else:
-        senha_criptografada = bcrypt_context.hash(senha)
-        novo_usuario = Usuario(nome, email, senha_criptografada)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
         return {"message": "Usuário cadastrado com sucesso"}
