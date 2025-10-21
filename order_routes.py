a6d5ba0f2c03611e69085c943a11804782daa7ce
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-10-21 19:45:05 -0400

    feat:adiciona base do criar pedido

--- order_routes.py
+++ order_routes.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import get_session
+from schemas import PedidoSchema
+from models import Pedido
 
 order_router = APIRouter(prefix="/orders", tags=["orders"])
 
-@order_router.get("/")
+@order_router.get("/pedido")
 async def get_orders():
     return {"message": "List of orders"}
+
+@order_router.post("/pedido")
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(get_session)):
+    novo_pedido = Pedido(usuario=pedido_schema.usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {"message": f"Order created successfully id = {novo_pedido.id}"}
