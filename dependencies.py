cfb6c6278e6a51674dd5d17ad8d0eac15c61837c
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-10-27 19:36:50 -0400

    feat: adiciona verificar_token como uma dependencia

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,9 @@
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker, Session 
 from models import db
+from models import Usuario
+from fastapi import Depends, HTTPException
+from jose import jwt, JWTError
+from main import SECRET_KEY, ALGORITHM, oauth2_schema  
 
 def get_session():
     try:
@@ -8,3 +12,16 @@ def get_session():
         yield session
     finally:
         session.close() 
+
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(get_session)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id_usuario = dic_info.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token inválido ou expirado")
+    usuario = session.query(Usuario).filter(Usuario.id==id_usuario).first()
+
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso inválido")
+    return usuario
+
