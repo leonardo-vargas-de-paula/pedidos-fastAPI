f50a9e37b0f2eb33b2f8b7149b395658c3c62559
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-10-28 17:29:48 -0400

    feat: adiciona base do cancelar pedido

--- order_routes.py
+++ order_routes.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,HTTPException
 from sqlalchemy.orm import Session
-from dependencies import get_session
+from dependencies import get_session, verificar_token
 from schemas import PedidoSchema
 from models import Pedido
 
-order_router = APIRouter(prefix="/orders", tags=["orders"])
+order_router = APIRouter(prefix="/orders", tags=["orders"], dependencies = [Depends(verificar_token)])
 
-@order_router.get("/pedido")
+@order_router.get("/")
 async def get_orders():
-    return {"message": "List of orders"}
+    return {"message": "Pedidos"}
 
 @order_router.post("/pedido")
 async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(get_session)):
@@ -16,3 +16,17 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(g
     session.add(novo_pedido)
     session.commit()
     return {"message": f"Order created successfully id = {novo_pedido.id}"}
+
+@order_router.put("/pedido/cancelar/{id_pedido}")
+async def calcelar_pedido(id_pedido: int, session: Session = Depends(get_session)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+
+    if not pedido:
+        raise HTTPException(status_code=404, detail="Pedido nao encontrado")
+    pedido.status = "CANCELADO"
+    session.commit()
+
+    return {
+        "message": f"Pedido {id_pedido} cancelado com sucesso",
+        "pedido": pedido
+    }
